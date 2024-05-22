1eb5c8a34fdc4d94dab5437a1c355eef284d8102
larry <larrytientcheu1@gmail.com>
2024-05-22 18:46:29 +0200

    helper function created

--- ai_model/helpers/helpers.py
+++ ai_model/helpers/helpers.py
@@ -1,14 +1,34 @@
+from pathlib import Path
 import joblib
-import numpy as np
+from joblib import Memory
 import pandas as pd
+import time
+
+memory = Memory(Path(__file__).parent / ".cachedir", verbose=0)
 
 
-def make_predictions(model_path, encoder_path, inputs):
-    model = joblib.load_model(model_path)
-    encoder = joblib.load_encoder(encoder_path)
+# Function to load pickle files
+def load_pickles():
+    # Configure the path to be the grand parent directory of helpers.py
+    path = Path(__file__).parent.parent
 
-    for input in inputs:
-        input_df = pd.DataFrame([input])
+    model_path = path / "tmodels/nn_model.pkl"
+    label_encoder_path = path / "tmodels/label_encoders.pkl"
+
+    model = joblib.load(model_path)
+    encoder = joblib.load(label_encoder_path)
+    return model, encoder
+
+
+# Caching function to reduce model load time
+def cached_load():
+    loaded = memory.cache(load_pickles)
+    return loaded()
+
+
+def make_prediction(inputs):
+    model, encoder = cached_load()  # type: ignore
+    input_df = pd.DataFrame([inputs])
 
     categorical_features = ["AIRLINE", "ORIGIN_AIRPORT", "DESTINATION_AIRPORT"]
 
@@ -31,18 +51,20 @@ def make_predictions(model_path, encoder_path, inputs):
     # Drop the original datetime columns
     input_df = input_df.drop(["SCHEDULED_DEPARTURE"], axis=1)
 
-    input_df = input_df[
-        [
-            "AIRLINE",
-            "ORIGIN_AIRPORT",
-            "DESTINATION_AIRPORT",
-            "SCHEDULED_DEPARTURE_HOUR",
-            "SCHEDULED_DEPARTURE_MINUTE",
-            "SCHEDULED_DEPARTURE_DAY_OF_WEEK",
-            "SCHEDULED_DEPARTURE_MONTH",
-        ]
-    ]
+    prediction = model.predict(input_df)
 
-    predictions = model.predict(input_df)
+    return prediction[0][0]
 
-    return predictions
+
+# Helper Test function
+# data = {
+#     "AIRLINE": "AS",
+#     "SCHEDULED_DEPARTURE": "2023-12-28 06:23:00",
+#     "ORIGIN_AIRPORT": "MIA",
+#     "DESTINATION_AIRPORT": "JFK",
+# }
+# start = time.time()
+# price = make_prediction(data)
+# end = time.time()
+# print("\nThe function took {:.2f} s to compute.".format(end - start))
+# print("\nThe price is:\n {}".format(price))
